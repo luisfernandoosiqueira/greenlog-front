676e9548e92a2337df5dc67766c531d3340e4f3c
Luis Fernando <luisfernandosiqueiraadv@gmail.com>
2025-12-07 21:04:26 -0300

    feat: ajustes em auth, login e services

--- src/app/services/itinerario.service.ts
+++ src/app/services/itinerario.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ItinerarioRequest, ItinerarioResponse } from "../model/Itinerario";
 import { catchError, Observable, throwError } from "rxjs";
@@ -6,34 +6,44 @@ import { catchError, Observable, throwError } from "rxjs";
 @Injectable({
   providedIn: 'root'
 })
+export class ItinerarioService {
+  private apiUrl = 'http://localhost:8080/api/itinerarios';
 
-export class ItinerarioService{
-    private apiUrl = 'http://localhost:8080/api/itinerarios';
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {}
+  findAll(): Observable<ItinerarioResponse[]> {
+    return this.http
+      .get<ItinerarioResponse[]>(this.apiUrl)
+      .pipe(catchError(this.handleError));
+  }
 
-    findAll(): Observable<ItinerarioResponse[]> {
-        return this.http.get<ItinerarioResponse[]>(this.apiUrl).pipe(catchError(this.handleError));
-    }
+  findByData(id: number): Observable<ItinerarioResponse> {
+    return this.http
+      .get<ItinerarioResponse>(`${this.apiUrl}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
 
-    findByData(id: number): Observable<ItinerarioResponse> {
-        return this.http.get<ItinerarioResponse>(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError));
-    }
+  create(novoItinerario: ItinerarioRequest): Observable<ItinerarioResponse> {
+    return this.http
+      .post<ItinerarioResponse>(this.apiUrl, novoItinerario)
+      .pipe(catchError(this.handleError));
+  }
 
-    create(novoItinerario: ItinerarioRequest): Observable<ItinerarioResponse> {
-        return this.http.post<ItinerarioResponse>(this.apiUrl, novoItinerario).pipe(catchError(this.handleError));
-    }
+  update(atualizarItinerario: ItinerarioRequest, id: number): Observable<ItinerarioResponse> {
+    return this.http
+      .put<ItinerarioResponse>(`${this.apiUrl}/${id}`, atualizarItinerario)
+      .pipe(catchError(this.handleError));
+  }
 
-    update(atualizarItinerario: ItinerarioRequest, id: number): Observable<ItinerarioResponse> {
-        return this.http.put<ItinerarioResponse>(`${this.apiUrl}/${id}`, atualizarItinerario).pipe(catchError(this.handleError));
-    }
+  delete(id: number): Observable<ItinerarioResponse> {
+    return this.http
+      .delete<ItinerarioResponse>(`${this.apiUrl}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
 
-    delete(id: number): Observable<ItinerarioResponse> {
-        return this.http.delete<ItinerarioResponse>(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError));
-    }
-
-    private handleError(error: any) {
-        console.error('Erro na API bairros: ', error);
-        return throwError(() => new Error('Erro ao consultar a API bairro.'));
-    }
+  // propaga o erro original para que o componente possa ler a mensagem do backend
+  private handleError(error: HttpErrorResponse) {
+    console.error('Erro na API de itinerários: ', error);
+    return throwError(() => error);
+  }
 }
